00140fce1f5b09d1ebf26aa82268adb98dfb6c46
mudas-ir <mudassirkhan1160@gmail.com>
2025-12-15 10:20:16 +0530

    add more projects

--- 013_Program_to_Interface_Temperature(LM35)_Sensor_&_LDR/Objects/main.d
+++
@@ -1,2 +0,0 @@
-./objects/main.o: main.c \
-  C:\Users\mohammed.hussain\Desktop\Mudassir\TivaWare_C_Series-2.2.0.295\inc\tm4c123gh6pm.h
--- 013_Program_to_Interface_Temperature(LM35)_Sensor_&_LDR/Objects/startup_rvmdk-1.d
+++
@@ -1,2 +0,0 @@
-./objects/startup_rvmdk-1.o: \
-  C:\Users\mohammed.hussain\Desktop\Mudassir\PROGRAMS-NEW\ BOARD\startup_rvmdk-1.S
